6e0f768e9fd0f5fe2efe4e99c5f9d32cd9bf9c4e
Francisco A. C. Souza <francisceioseph@gmail.com>
2016-11-24 22:49:21 -0300

    Uso de Middlewares Roteamento usando middlewares do express - Rota de hello - Rota de greet with query

--- hello-express/index.js
+++ hello-express/index.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const app = express();
+
+// Gets a greeting in the '/hello'
+app.get('/hello', (request, response) => {
+    response.send('Welcome to Hello Express!!!');
+});
+
+//greet an user based in a query param
+app.get('/greet', (request, response) => {
+    response.send(`Hello ${request.query.nome}!`);
+});
+
+
+// Listen to requests on 8080 port
+app.listen(8080);
