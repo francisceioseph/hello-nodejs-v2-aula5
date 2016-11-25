07816be92f000ce7b1784a402e2638ce4984d7e5
Francisco A. C. Souza <francisceioseph@gmail.com>
2016-11-24 22:57:11 -0300

    Uso de queryparams

--- hello-express/index.js
+++ hello-express/index.js
@@ -11,6 +11,9 @@ app.get('/greet', (request, response) => {
     response.send(`Hello ${request.query.nome}!`);
 });
 
-
+app.get('/queryparams', (request, response) => {
+    console.log(request.query);
+    response.send('See your console...');
+})
 // Listen to requests on 8080 port
 app.listen(8080);
